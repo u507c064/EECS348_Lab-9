bd5ceba336bf9262fc0d11eacfe8ff674d7835c8
Harlan Williams <harlanrhwilliams@gmail.com>
2025-04-01 09:11:24 -0500

    Create matrix.hpp

---
+++ matrix.hpp
@@ -0,0 +1,26 @@
+#ifndef __MATRIX_HPP__
+#define __MATRIX_HPP__
+
+#include <cstdint>
+#include <vector>
+
+// modify this class declaration to take a template for the type of the data it holds
+
+class Matrix {
+public:
+    Matrix(std::size_t N);
+    Matrix(std::vector<std::vector<int>> nums);
+
+    Matrix operator+(const Matrix &rhs) const;
+    Matrix operator*(const Matrix &rhs) const;
+    void set_value(std::size_t i, std::size_t j, int n);
+    int get_value(std::size_t i, std::size_t j) const;
+    int get_size() const;
+    int sum_diagonal_major() const;
+    int sum_diagonal_minor() const;
+    void swap_rows(std::size_t r1, std::size_t r2);
+    void swap_cols(std::size_t c1, std::size_t c2);
+    void print_matrix() const;
+};
+
+#endif // __MATRIX_HPP__
